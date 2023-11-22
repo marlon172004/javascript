ad22f073bbb003bf5fd731ba1d4b59299a36d48f
marlon172004 <129074600+marlon172004@users.noreply.github.com>
2023-11-22 17:59:30 -0500

    codigo comentado
    instructora este es el la primera actvidad de javascript

--- javascript/primer trabajos/js/holaMundo.js
+++ javascript/primer trabajos/js/holaMundo.js
@@ -1,17 +1,21 @@
-nacimiento=parseInt(prompt("Ingresa tu año de Nacimiento"));
+// Solicita al usuario que ingrese su año de nacimiento y lo almacena en la variable 'nacimiento'
+const nacimiento = parseInt(prompt("Ingresa tu año de Nacimiento"));
 
-
+// Obtiene la fecha y hora actuales y almacena el año actual en la variable 'fechaActual'
 const fecha = new Date();
-const fechaActual=fecha.getFullYear();
-let edad = fechaActual-nacimiento;
+const fechaActual = fecha.getFullYear();
 
+// Calcula la edad restando el año de nacimiento al año actual
+const edad = fechaActual - nacimiento;
 
-var nombres= prompt("Ingresa tu Nombre");
-//var Edad= parseInt(prompt("Ingresa tu Edad"));//
+// Solicita al usuario que ingrese su nombre y lo almacena en la variable 'nombres'
+const nombres = prompt("Ingresa tu Nombre");
 
-if(edad<=8) {
-document.write("Bienvenidos niño(a) "+nombres+" usted tiene "+edad+" años de edad")
-}else if(edad<=30) {
-    document.write("Bienvenidos joven(a) "+nombres+" usted tiene "+edad+" años de edad")
-}else{document.write("Bienvenidos señor(a) "+nombres+" usted tiene "+edad+" años de edad")
+// Realiza comparaciones para determinar la categoría de edad del usuario y muestra un mensaje de bienvenida personalizado
+if (edad <= 8) {
+    console.log("Bienvenidos niño(a) " + nombres + ". Usted tiene " + edad + " años de edad.");
+} else if (edad <= 30) {
+    console.log("Bienvenidos joven(a) " + nombres + ". Usted tiene " + edad + " años de edad.");
+} else {
+    console.log("Bienvenidos señor(a) " + nombres + ". Usted tiene " + edad + " años de edad.");
 }
