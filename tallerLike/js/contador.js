84cf59b5fd0255ba3c38f43fa13eead5031a13bb
marlon172004 <129074600+marlon172004@users.noreply.github.com>
2023-11-28 20:45:27 -0500

    contador
    actualizado

--- tallerLike/js/contador.js
+++ tallerLike/js/contador.js
@@ -1,28 +1,33 @@
+// Escucha el evento 'keydown' en la ventana y ejecuta la función correspondiente
 window.addEventListener('keydown', (e) => {
+    // Comprueba si la tecla presionada es '+'
     if (e.key === '+') {
-        incremento();
+        incremento(); // Llama a la función incremento
     } else if (e.key === '-') {
-        decremento();
+        decremento(); // Llama a la función decremento
     }
 });
 
+// Función para incrementar el contador
 const incremento = () => {
-    let contador = parseInt(document.getElementById('valor').innerHTML);
-    contador++;
-    document.getElementById('valor').innerHTML = contador;
+    let contador = parseInt(document.getElementById('valor').innerHTML); // Obtiene el valor actual del contador
+    contador++; // Incrementa el contador
+    document.getElementById('valor').innerHTML = contador; // Actualiza el contenido HTML con el nuevo valor del contador
 };
 
+// Función para decrementar el contador
 const decremento = () => {
-    let contador = parseInt(document.getElementById('valor').innerHTML);
+    let contador = parseInt(document.getElementById('valor').innerHTML); // Obtiene el valor actual del contador
     if (contador > 0) {
-        contador--;
-        document.getElementById('valor').innerHTML = contador;
+        contador--; // Decrementa el contador si es mayor que cero
+        document.getElementById('valor').innerHTML = contador; // Actualiza el contenido HTML con el nuevo valor del contador
     } else {
-        contador = 0;
-        document.getElementById('valor').innerHTML = contador;
+        contador = 0; // Establece el contador en cero si ya es cero
+        document.getElementById('valor').innerHTML = contador; // Actualiza el contenido HTML con el nuevo valor del contador
     }
 };
 
+// Función para resetear el contador a cero
 const resetear = () => {
-    document.getElementById('valor').innerHTML = '0';
+    document.getElementById('valor').innerHTML = '0'; // Establece el contenido HTML del contador en cero
 };
